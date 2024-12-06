ebf574e29f218e3a94068490ad7da10ae630082e
TayyabRaza <159689011+Tayyab-Raza@users.noreply.github.com>
2024-12-06 18:20:23 +0530

    Update

--- client/src/i18next.js
+++ client/src/i18next.js
@@ -3,8 +3,8 @@ import HttpBackend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 
-const apiKey = "ty9f2OtSRbRitb5p2Gd6Aw";
-const loadPath = `https://api.i18nexus.com/project_resources/translations/{{lng}}/{{ns}}.json?api_key=${apiKey}`;
+const apiKey = "";
+const loadPath = ``;
 
 i18next.use(HttpBackend).use(LanguageDetector).use(initReactI18next).init({
     fallbackLng: "en",
